8ca172c71ae5f67de9719801210e0f1679bcb1a6
AndyImaI <149210474+AndyImaI@users.noreply.github.com>
2023-11-29 15:23:13 -0300

    criando primitivos

---
+++ tarefa_wrapper/tarefamod9/src/PrimeiraClasse.java
@@ -0,0 +1,31 @@
+public class PrimeiraClasse {
+
+    public static void main (String args[]){
+
+        int i = 15;
+
+        int it = 25;
+
+        short s = 20;
+
+        byte b = 3;
+
+        boolean p = true;
+
+        double d = 40.4;
+
+        float f = 5f;
+
+        char c = 'a';
+
+        long l = 6;
+
+
+        float num1 = (float) i  / it ;
+
+        System.out.println(num1);
+
+
+    }
+    
+}
